1db556e58eac1af824b1ba8e7c4640bc64934208
Developer <dev@dev.local>
2019-12-20 11:24:00 +0000

    Added password validators

--- src/app/admin/services/validator.ts
+++ src/app/admin/services/validator.ts
@@ -1,19 +1,29 @@
-import { Validators, ValidatorFn, AsyncValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
+import { FormGroup, Validators, ValidatorFn, AsyncValidatorFn, ValidationErrors, AbstractControl, FormControl } from '@angular/forms';
 import { Observable, from } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { UsersService} from './users.service'
+import { UsersService } from './users.service';
 
 export class Validator {
     static password(): ValidatorFn {
-        return Validators.compose([Validators.required, Validators.minLength(2)]);
+        return Validators.compose([Validators.required, Validators.minLength(3)]);
     }
 
     static username(): ValidatorFn {
-        return Validators.compose([Validators.required, Validators.minLength(4), usernamePatternValidator]);
+        return Validators.compose([Validators.required, Validators.minLength(3), usernamePatternValidator]);
     }
 
     static usernameAsync(usersService: UsersService): AsyncValidatorFn {
         return Validators.composeAsync([existingUsernameValidator(usersService)]);
+    }
+
+    static passwordEqual(password1: string, password2: string): ValidatorFn {
+        return (control: FormGroup): ValidationErrors | null => {
+            const p1 = control.get(password1);
+            const p2 = control.get(password2);
+            const error = p1.value !== p2.value ? { notEqual: 'Parolēm jāsakrīt' } : null;
+            p2.setErrors(error);
+            return error;
+        };
     }
 }
 
--- src/app/admin/users/user-editor/password-change-dialog/password-change-dialog.component.ts
+++ src/app/admin/users/user-editor/password-change-dialog/password-change-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { Validator } from "../../../services/validator";
 
 @Component({
   selector: 'app-password-change-dialog',
@@ -11,9 +12,10 @@ export class PasswordChangeDialogComponent implements OnInit {
 
   @Inject(MAT_DIALOG_DATA) data: { username: string; };
   passwordForm = new FormGroup({
-    password: new FormControl(),
-    password2: new FormControl(),
-  });
+    password: new FormControl('', Validator.password()),
+    password2: new FormControl(''),
+  },{validators: Validator.passwordEqual('password', 'password2')});
+  password = this.passwordForm.get('password');
   constructor(
     private dialogRef: MatDialogRef<PasswordChangeDialogComponent>
   ) { }
@@ -24,5 +26,5 @@ export class PasswordChangeDialogComponent implements OnInit {
   onCancel() {
     this.dialogRef.close();
   }
-  // TODO validators
+
 }
