190596c58eac36e821b135403b330f7273e1957a
Developer <dev@dev.local>
2019-12-22 15:09:31 +0000

    new user deactivate guard

--- src/app/admin/users/new-user/new-user.component.ts
+++ src/app/admin/users/new-user/new-user.component.ts
@@ -7,13 +7,16 @@ import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Validator } from '../../services/validator';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { ConfirmationDialogComponent } from "../../../library/confirmation-dialog/confirmation-dialog.component";
+import { CanComponentDeactivate } from "../../../library/guards/can-deactivate.guard";
 
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
   styleUrls: ['./new-user.component.css']
 })
-export class NewUserComponent implements OnInit {
+export class NewUserComponent implements OnInit, CanComponentDeactivate {
 
   newUserForm = new FormGroup({
     username: new FormControl(null, Validator.username(), Validator.usernameAsync(this.usersService)),
@@ -34,6 +37,7 @@ export class NewUserComponent implements OnInit {
     private usersService: UsersService,
     private router: Router,
     private snackBar: MatSnackBar,
+    private dialog: MatDialog,
   ) { }
 
   ngOnInit() {
@@ -44,12 +48,27 @@ export class NewUserComponent implements OnInit {
     const username = this.username.value;
     this.usersService.addUser(this.newUserForm.value).subscribe(resp => {
       if (resp) {
+        this.newUserForm.markAsPristine();
         this.snackBar.open(`Lietotājs ${username} izveidots!`, 'OK', { duration: 3000 });
         this.router.navigate(['admin', 'users', { id: username }]);
       } else {
         this.snackBar.open(`Neizdevās izveidot lietotāju`, 'OK', { duration: 5000 });
       }
     });
+  }
+
+  canDeactivate(): Observable<boolean> | boolean {
+    if (this.newUserForm.pristine) {
+      return true;
+    }
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: {
+        prompt: 'Vai tiešām vēlaties pamest nesaglabātu?',
+        yes: 'Jā, pamest!',
+        no: 'Nē, turpināt!',
+      }
+    })
+    return dialogRef.afterClosed();
   }
 
 }
