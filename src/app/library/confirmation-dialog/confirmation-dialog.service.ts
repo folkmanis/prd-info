a2f19ef96cf9a7aee3f796bfeb4dc11d81dc050a
Developer <dev@dev.local>
2020-02-08 15:23:13 +0000

    Confirmation dialog service

---
+++ src/app/library/confirmation-dialog/confirmation-dialog.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { MatDialog, MatDialogConfig, MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { ConfirmationDialogComponent } from "./confirmation-dialog.component";
+import { switchMap, filter } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfirmationDialogService {
+
+  constructor(
+    private dialog: MatDialog,
+
+  ) { }
+
+  confirm(prompt: string, config: MatDialogConfig = {}): Observable<boolean> {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: { prompt }, ...config
+    });
+    return dialogRef.afterClosed();
+  }
+  
+}
