dd0658f2120f7387263f994621aeaf97ee7222fe
Developer <dev@dev.local>
2020-02-20 10:18:29 +0000

    Fixed pasutijums selector

--- src/app/kastes/select-pasutijums/select-pasutijums.component.ts
+++ src/app/kastes/select-pasutijums/select-pasutijums.component.ts
@@ -1,38 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PasutijumiService } from '../services/pasutijumi.service';
 import { KastesPreferencesService } from '../services/kastes-preferences.service';
-import { switchMap, distinctUntilChanged, map, tap, skip, take } from 'rxjs/operators';
+import { switchMap, distinctUntilChanged, map, tap, skip, take, takeUntil } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-select-pasutijums',
   templateUrl: './select-pasutijums.component.html',
   styleUrls: ['./select-pasutijums.component.css']
 })
-export class SelectPasutijumsComponent implements OnInit {
+export class SelectPasutijumsComponent implements OnInit, OnDestroy {
 
 
   constructor(
     private pasutijumiService: PasutijumiService,
     private kastesPreferencesService: KastesPreferencesService,
     private router: Router,
-    ) { }
+  ) { }
   pasControl = new FormControl();
   pasutijumi$ = this.pasutijumiService.pasutijumi.pipe(
     map(pas => pas.filter(p => !p.deleted)) // Dzēstos nerāda
   );
+  private subs: Subscription;
 
   ngOnInit() {
+    // Sākotnējā vērtība
     this.kastesPreferencesService.preferences.pipe(
-      take(1),
-    ).subscribe(pref => this.pasControl.setValue(pref.pasutijums));
-    this.pasControl.valueChanges.pipe(
-      skip(1),
-      distinctUntilChanged(),
+      take(1), // Nevajag atrakstīties
+      map(pref => pref.pasutijums),
+    ).subscribe(pas => this.pasControl.setValue(pas, { emitEvent: false }));
+    // Mainās pasūtījums
+    this.subs = this.pasControl.valueChanges.pipe(
       switchMap(val => this.pasutijumiService.setPasutijums(val as string)),
-      tap(()=> this.router.navigate(['kastes','selector',0])),
+      tap(() => this.router.navigate(['kastes', 'selector', 0])),
     ).subscribe();
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 
 }
