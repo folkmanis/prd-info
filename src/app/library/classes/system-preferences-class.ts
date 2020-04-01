769fb0df9af831f87670d894e53f0b84051ce40d
Developer <dev@dev.local>
2020-04-01 06:43:26 +0000

    jobs default settings added

--- src/app/library/classes/system-preferences-class.ts
+++ src/app/library/classes/system-preferences-class.ts
@@ -11,7 +11,7 @@ export type SystemPreferencesGroups = 'kastes' | 'system' | 'jobs';
 
 export type SystemPreferences = Map<SystemPreferencesGroups, ModuleSettings>;
 
-export interface KastesSettings {
+export class KastesSettings implements ModuleSettings {
     colors: {
         yellow: string;
         rose: string;
@@ -24,8 +24,17 @@ export class SystemSettings implements ModuleSettings {
     logLevels: [number, string][];
 }
 
+export class JobsSettings implements ModuleSettings {
+    productCategories: Array<{ category: string, description: string; }>;
+}
+
 export const DEFAULT_SYSTEM_PREFERENCES: SystemPreferences = new Map<SystemPreferencesGroups, ModuleSettings>()
-    .set('kastes',
-        { colors: { yellow: 'hsl(45,75%,60%)', rose: '315,75%,50%', white: '0,0%,50%', } })
-    .set('system', { menuExpandedByDefault: false })
-    .set('jobs', {});
+    .set('kastes', <KastesSettings>{
+        colors: { yellow: 'hsl(45,75%,60%)', rose: '315,75%,50%', white: '0,0%,50%', },
+    })
+    .set('system', <SystemSettings>{ menuExpandedByDefault: false })
+    .set('jobs', <JobsSettings>{
+        productCategories: [
+            { category: 'plates', description: 'Iespiedformas' }
+        ]
+    });
