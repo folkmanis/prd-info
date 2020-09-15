5c6a956ad3e400b878d9db3b25cd9fd1fd0772b9
Developer <dev@dev.local>
2020-09-15 09:05:29 +0000

    unsubscribe uses destroy service

--- src/app/kastes/select-tabula/selector/selector.component.ts
+++ src/app/kastes/select-tabula/selector/selector.component.ts
@@ -2,15 +2,17 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { KastesPreferencesService } from '../../services/kastes-preferences.service';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Observable, pipe, Subscription } from 'rxjs';
-import { map, tap, filter, switchMap, distinctUntilChanged } from 'rxjs/operators';
+import { map, tap, filter, switchMap, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 import { KastesTabulaService } from '../services/kastes-tabula.service';
+import { DestroyService } from 'src/app/library/rx/destroy.service';
 
 @Component({
   selector: 'app-selector',
   templateUrl: './selector.component.html',
-  styleUrls: ['./selector.component.css']
+  styleUrls: ['./selector.component.css'],
+  providers: [DestroyService],
 })
-export class SelectorComponent implements OnInit, OnDestroy {
+export class SelectorComponent implements OnInit {
   selected: number;
 
   constructor(
@@ -18,31 +20,28 @@ export class SelectorComponent implements OnInit, OnDestroy {
     private router: Router,
     private preferencesService: KastesPreferencesService,
     private tabulaService: KastesTabulaService,
+    private destroy$: DestroyService,
   ) { }
 
   usrPref$ = this.preferencesService.kastesUserPreferences$;
-
-  private readonly _subs = new Subscription();
 
   apjomi$: Observable<number[]> = this.tabulaService.apjomi$.pipe(
     map(apj => [0, ...apj]),
   );
 
   ngOnInit() {
-    this._subs.add(
-      this.route.paramMap.pipe(
-        map(params => +params.get('apjoms')),
-        tap(apjoms => this.selected = apjoms),
-      )
-        .subscribe(this.tabulaService.apjoms$)
-    );
+    this.route.paramMap.pipe(
+      map(params => +params.get('apjoms')),
+      tap(apjoms => this.selected = apjoms),
+      takeUntil(this.destroy$),
+    )
+      .subscribe(this.tabulaService.apjoms$);
 
-    this.tabulaService.pasutijumsId$.subscribe(
+    this.tabulaService.pasutijumsId$.pipe(
+      takeUntil(this.destroy$),
+    ).subscribe(
       () => this.router.navigate(['../0'], { relativeTo: this.route })
     );
   }
 
-  ngOnDestroy(): void {
-    this._subs.unsubscribe();
-  }
 }
