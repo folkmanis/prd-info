247fece8dfba245d1983d1a1ed9d64494d023977
Developer <dev@dev.local>
2020-09-27 15:54:09 +0000

    basic functionality

--- src/app/jobs/job-edit/folder-path/folder-path.component.ts
+++ src/app/jobs/job-edit/folder-path/folder-path.component.ts
@@ -1,47 +1,76 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
-import { JobBase } from 'src/app/interfaces';
-import { getYear, format } from 'date-fns';
-import { lv } from 'date-fns/locale';
+import { ChangeDetectionStrategy, Component, Input, OnInit, Self } from '@angular/core';
+import { NgControl } from '@angular/forms';
+import { MatCheckboxChange } from '@angular/material/checkbox';
+import { IControlValueAccessor } from '@rxweb/types';
 
-function capitalize(s: string): string {
-  if (typeof s !== 'string') { return ''; }
-  return s[0].toUpperCase() + s.slice(1);
-}
-function removeDiactrics(s: string): string {
-  return s.normalize('NFKD').replace(/[\u0300-\u036F]/g, '');
-}
-
-function toMonthNumberName(date: Date): string {
-  return new Intl.DateTimeFormat(undefined, { month: '2-digit' }).format(date)
-    + '-'
-    + capitalize(removeDiactrics(new Intl.DateTimeFormat(undefined, { month: 'long' }).format(date)));
-}
 
 @Component({
   selector: 'app-folder-path',
   templateUrl: './folder-path.component.html',
-  styleUrls: ['./folder-path.component.css']
+  styleUrls: ['./folder-path.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FolderPathComponent implements OnInit {
-
-  private _date: Date;
-  @Input('date') set date(param: Date | string) {
-    this._date = new Date(param);
-    if (this._date) {
-      console.log(this.pathArray(this._date));
+export class FolderPathComponent implements OnInit, IControlValueAccessor<string[]> {
+  @Input() set defaultPath(val: string[]) {
+    this._defaultPath = val;
+    console.log('input', val, this.path, this.initialPath, this.checked);
+    if (!this.path || !this.initialPath || !this.checked) {
+      this.path = this.defaultPath;
+      if (this.checked) {
+        this.setValueFn(this.path);
+      }
     }
   }
+  get defaultPath(): string[] { return this._defaultPath; }
+  private _defaultPath: string[] = [];
 
-  constructor() { }
+  set checked(val: boolean) { this._checked = val; }
+  get checked(): boolean { return this._checked; }
+  private _checked = false;
+
+  isDisabled = false;
+  path: string[] | undefined;
+  initialPath: string[] | undefined;
+
+  setValueFn: (val: string[]) => void;
+
+  constructor(
+    @Self() private ngControl: NgControl,
+  ) {
+    this.ngControl.valueAccessor = this;
+  }
+
+  writeValue(obj: string[]) {
+    console.log('writeValue');
+    if (!this.initialPath) { this.initialPath = obj; }
+    this.path = obj || this.defaultPath;
+    this.checked = obj && obj.length > 0;
+  }
+
+  registerOnChange(fn: (val: string[]) => void) {
+    this.setValueFn = fn;
+  }
+
+  registerOnTouched(fn: any) {
+  }
+
+  setDisabledState(isDisabled: boolean) {
+    console.log('setDisabledState');
+    this.isDisabled = isDisabled;
+  }
+
+  onChange({ checked }: MatCheckboxChange) {
+    this.checked = checked;
+    this.setValueFn(this.checked ? this.path : undefined);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit');
   }
 
-  private pathArray(date: Date): string[] {
-    return [
-      getYear(date).toString(),
-      toMonthNumberName(date),
-    ];
+  get pathname(): string {
+    return this.path?.join('/') || '';
   }
+
 
 }
