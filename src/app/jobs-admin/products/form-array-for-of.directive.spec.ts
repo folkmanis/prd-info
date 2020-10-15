7482cada1c47284df4e9914caa8f963afdb87e08
Developer <dev@dev.local>
2020-10-15 05:54:35 +0000

    removed directive

--- src/app/jobs-admin/products/form-array-for-of.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormArrayForOfDirective } from './form-array-for-of.directive';
-
-describe('FormArrayForOfDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FormArrayForOfDirective();
-    expect(directive).toBeTruthy();
-  });
-});
