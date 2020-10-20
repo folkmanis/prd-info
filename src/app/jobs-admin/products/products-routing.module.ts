a47400db749e0052b86399cb639018d3175100df
Developer <dev@dev.local>
2020-10-20 06:29:19 +0000

    refactor for secondary outlet

--- src/app/jobs-admin/products/products-routing.module.ts
+++ src/app/jobs-admin/products/products-routing.module.ts
@@ -28,6 +28,7 @@ const routes: Routes = [
         path: ':id',
         component: EditComponent,
         canDeactivate: [CanDeactivateGuard],
+        outlet: 'editor',
         resolve: {
           product: ProductsResolverService
         }
