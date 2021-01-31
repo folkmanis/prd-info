a1ab245e0547829997a34890768f350f9a4c89f5
Edmunds Folkmanis <folkmanis@gmail.com>
2021-01-31 14:30:53 +0000

    a bunch of small improvements

--- src/app/admin/module-preferences/modules/jobs-preferences/job-categories/job-categories.component.ts
+++ src/app/admin/module-preferences/modules/jobs-preferences/job-categories/job-categories.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, Input, OnDestroy } from '@angular/core';
 import { IFormBuilder, IControlValueAccessor, IFormArray } from '@rxweb/types';
 import { JobsSettings, ProductCategory } from 'src/app/interfaces';
 import { ControlValueAccessor, FormBuilder, NgControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { EMPTY, of, BehaviorSubject, Subject, Observable, merge } from 'rxjs';
-import { map, mergeAll, mergeMap, share, shareReplay, takeUntil, tap } from 'rxjs/operators';
+import { map, mergeAll, mergeMap, share, shareReplay, skip, takeUntil, tap } from 'rxjs/operators';
 import { CategoryDialogComponent } from './category-dialog/category-dialog.component';
 import { DestroyService } from 'src/app/library/rx';
+
+enum Action { ADD, REMOVE, UPDATE }
+interface UpdateAction<T> { type: Action; data?: T; idx?: number; }
 
 @Component({
   selector: 'app-job-categories',
@@ -15,42 +18,35 @@ import { DestroyService } from 'src/app/library/rx';
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [DestroyService],
 })
-export class JobCategoriesComponent implements OnInit, IControlValueAccessor<ProductCategory[]> {
+export class JobCategoriesComponent implements OnInit, OnDestroy, IControlValueAccessor<ProductCategory[]> {
+@Input() columns: string[] = [];
 
   private updateFn: (obj: ProductCategory[]) => void;
   private touchedFn: () => void;
 
   displayedColumns: (keyof ProductCategory | 'button')[] = ['button', 'category', 'description'];
 
+  disabled = false;
+
   private _initialCategories$ = new BehaviorSubject<ProductCategory[]>([]);
-
-  disabled$ = new BehaviorSubject<boolean>(false);
-
-  newCategorie$ = new Subject<ProductCategory>();
-  removeCategorie$ = new Subject<ProductCategory>();
-  updateCategorie$ = new Subject<ProductCategory>();
+  private _updateData$ = new Subject<UpdateAction<ProductCategory>>();
 
   categories$: Observable<ProductCategory[]> = this._initialCategories$.pipe(
     map(categories => merge(
       of(categories),
-      this.newCategorie$.pipe(
-        map(newCat => categories = [...categories, newCat]),
+      this._updateData$.pipe(
+        map(upd => categories = this.updateData(upd, categories))
       ),
-      this.removeCategorie$.pipe(
-        map(categorie => categories = categories.filter(c => c !== categorie)),
-      ),
-      this.updateCategorie$.pipe(
-        map(catUpd => categories = categories.map(c => c.category === catUpd.category ? catUpd : c)),
-      )
     )),
     mergeAll(),
-    shareReplay(),
+    shareReplay(1),
   );
 
   constructor(
     control: NgControl,
     private destroy$: DestroyService,
     private dialog: MatDialog,
+    private changeDetector: ChangeDetectorRef,
   ) {
     control.valueAccessor = this;
   }
@@ -68,26 +64,35 @@ export class JobCategoriesComponent implements OnInit, IControlValueAccessor<Pro
   }
 
   setDisabledState(isDisabled: boolean) {
-    this.disabled$.next(isDisabled);
+    this.disabled = isDisabled;
+    this.changeDetector.markForCheck();
   }
 
   ngOnInit(): void {
-    this.categories$.pipe(takeUntil(this.destroy$))
+    this.categories$.pipe(
+      skip(1),
+      takeUntil(this.destroy$)
+    )
       .subscribe(categories => this.updateFn(categories));
   }
 
-  onAddCategoy() {
+  ngOnDestroy(): void {
+    this._updateData$.complete();
+  }
+
+  onAddRow() {
     const dialogRef = this.dialog.open<CategoryDialogComponent, any, ProductCategory | undefined>(CategoryDialogComponent);
     dialogRef.afterClosed().pipe(
       tap(_ => this.touchedFn()),
       mergeMap(resp => resp ? of(resp) : EMPTY),
       takeUntil(this.destroy$),
     ).subscribe(cat => {
-      this.newCategorie$.next(cat);
+      this._updateData$.next({ type: Action.ADD, data: cat });
     });
   }
 
-  onEditCategory(obj: ProductCategory) {
+  onEditRow(obj: ProductCategory, idx: number) {
+    if (this.disabled) { return; }
     const dialogRef = this.dialog
       .open<CategoryDialogComponent, ProductCategory, ProductCategory | undefined>(CategoryDialogComponent, { data: obj });
     dialogRef.afterClosed().pipe(
@@ -95,13 +100,28 @@ export class JobCategoriesComponent implements OnInit, IControlValueAccessor<Pro
       mergeMap(resp => resp ? of(resp) : EMPTY),
       takeUntil(this.destroy$),
     ).subscribe(cat => {
-      this.updateCategorie$.next({ ...obj, ...cat });
+      this._updateData$.next({ type: Action.UPDATE, data: { ...obj, ...cat }, idx });
     });
   }
 
-  onRemoveCategory(obj: ProductCategory) {
-    this.removeCategorie$.next(obj);
+  onRemoveRow(idx: number) {
+    this._updateData$.next({ type: Action.REMOVE, idx });
     this.touchedFn();
   }
+
+  private updateData<T>({ type, data, idx }: UpdateAction<T>, dataArray: T[]): T[] {
+    if (type === Action.ADD) {
+      return [...dataArray, data];
+    }
+    if (type === Action.REMOVE && typeof idx === 'number') {
+      return dataArray.filter((_, i) => i !== idx);
+    }
+    if (type === Action.UPDATE && typeof idx === 'number' && data) {
+      return dataArray.map((d, i) => i === idx ? data : d);
+    }
+
+    return dataArray;
+  }
+
 
 }
