6c749c849903d2cdf8c1c4b21d975d7a926cb008
Edmunds Folkmanis <folkmanis@gmail.com>
2021-02-23 09:45:43 +0000

    removed unused test

--- src/app/admin/services/logfile-record.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LogfileRecord } from './logfile-record';
-
-describe('LogfileRecord', () => {
-  it('should create an instance', () => {
-    expect(new LogfileRecord()).toBeTruthy();
-  });
-});
