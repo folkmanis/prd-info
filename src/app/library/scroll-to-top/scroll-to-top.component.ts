d7e442c29b3b778497210adf09dc8948d6711fe1
Edmunds Folkmanis <folkmanis@gmail.com>
2021-03-05 15:52:13 +0000

    scroll-to-top refactor

--- src/app/library/scroll-to-top/scroll-to-top.component.ts
+++ src/app/library/scroll-to-top/scroll-to-top.component.ts
@@ -1,61 +1,20 @@
-import { Component, Input, OnInit, OnDestroy, NgZone, ElementRef, ChangeDetectorRef, HostBinding } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
-import { CdkScrollable, ScrollDispatcher } from '@angular/cdk/scrolling';
+import { CdkScrollable } from '@angular/cdk/scrolling';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-const showScrollHeight = 300;
-const hideScrollHeight = 10;
-
-const SCROLL_AUDIT_TIME = 200;
 
 @Component({
   templateUrl: './scroll-to-top.component.html',
-  styleUrls: ['./scroll-to-top.component.scss']
+  styleUrls: ['./scroll-to-top.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ScrollToTopComponent implements OnInit, OnDestroy {
+export class ScrollToTopComponent {
 
-  scrollable: CdkScrollable;
+  visible = false;
 
-  fixed: 'fixed' | null = null;
-
-  private readonly _subs = new Subscription();
-
-  showScroll = false;
-
-  constructor(
-    private zone: NgZone,
-    private dispatcher: ScrollDispatcher,
-    private chgRef: ChangeDetectorRef,
-  ) { }
-
-  ngOnInit(): void {
-    const sub = this.dispatcher.scrolled(SCROLL_AUDIT_TIME).pipe(
-      map(() => this.scrollable.measureScrollOffset('top')),
-    ).subscribe(top => {
-      if (!this.showScroll && top > showScrollHeight) {
-        /** Scroll tiek pārbaudīts ārpus zonas. Bez run nekādas reakcijas nebūs */
-        this.zone.run(() => {
-          this.showScroll = true;
-          this.chgRef.markForCheck();
-        });
-      } else if (this.showScroll && top < hideScrollHeight) {
-        /** Scroll tiek pārbaudīts ārpus zonas. Bez run nekādas reakcijas nebūs */
-        this.zone.run(() => {
-          this.showScroll = false;
-          this.chgRef.markForCheck();
-        });
-      }
-    });
-
-    this._subs.add(sub);
-  }
-
-  ngOnDestroy(): void {
-    this._subs.unsubscribe();
-  }
+  scrollable: CdkScrollable | undefined;
 
   scrollToTop() {
-    this.scrollable.scrollTo({ top: 0, behavior: 'smooth' });
+    this.scrollable?.scrollTo({ top: 0, behavior: 'smooth' });
   }
 
 }
--- src/app/library/scroll-to-top/scroll-top.directive.ts
+++ src/app/library/scroll-to-top/scroll-top.directive.ts
@@ -1,35 +1,60 @@
-import { ComponentFactoryResolver, ComponentRef, Directive, Host, Input, OnInit, Self, ViewContainerRef } from '@angular/core';
-import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import { CdkScrollable } from '@angular/cdk/scrolling';
+import { ChangeDetectorRef, ComponentFactoryResolver, ComponentRef, Directive, Host, OnInit, Self, ViewContainerRef } from '@angular/core';
+import { DestroyService } from 'prd-cdk';
+import { of } from 'rxjs';
+import { debounceTime, filter, map, switchMap, takeUntil, tap } from 'rxjs/operators';
 import { ScrollToTopComponent } from './scroll-to-top.component';
 
+const showScrollHeight = 300;
+const hideScrollHeight = 10;
+
+const SCROLL_AUDIT_TIME = 200;
+
 @Directive({
-  selector: '[appScrollTop], [cdk-scrollable], [scroll-to-top]'
+  selector: '[scroll-to-top]',
+  providers: [DestroyService],
 })
 export class ScrollTopDirective implements OnInit {
-  /** Button style.position is set to 'fixed'. Default = 'absolute' */
-  @Input() buttonPositionFixed: any;
 
-  private _componentRef: ComponentRef<ScrollToTopComponent> | undefined;
+  private factory = this.resolver.resolveComponentFactory(ScrollToTopComponent);
+  private componentRef: ComponentRef<ScrollToTopComponent> | null = null;
+  private changeDetectorRef: ChangeDetectorRef;
 
 
   constructor(
     private resolver: ComponentFactoryResolver,
     private container: ViewContainerRef,
     @Host() @Self() private scrollable: CdkScrollable,
+    private destroy$: DestroyService,
   ) { }
 
   ngOnInit(): void {
-    const factory = this.resolver.resolveComponentFactory(ScrollToTopComponent);
-    this._componentRef = this.container.createComponent(factory);
-    this._componentRef.instance.scrollable = this.scrollable;
-    this._componentRef.instance.fixed = (coerceBooleanProperty(this.buttonPositionFixed) && 'fixed') || null;
+
+    of(false).pipe(
+      switchMap(showScroll => this.scrollable.elementScrolled().pipe(
+        debounceTime(SCROLL_AUDIT_TIME),
+        map(() => this.scrollable.measureScrollOffset('top')),
+        map(offset => !showScroll && offset > showScrollHeight || showScroll && offset > hideScrollHeight),
+        filter(show => show !== showScroll),
+        tap(show => showScroll = show),
+        takeUntil(this.destroy$),
+      )),
+    ).subscribe(show => this.setVisible(show));
+
+  }
+
+  private setVisible(show: boolean) {
+    if (!this.componentRef) {
+      this.componentRef = this.container.createComponent(this.factory);
+      this.componentRef.instance.scrollable = this.scrollable;
+      this.changeDetectorRef = this.componentRef.injector.get(ChangeDetectorRef);
+    }
+    this.componentRef.instance.visible = show;
+    this.changeDetectorRef.detectChanges();
   }
 
   scrollToTop() {
-    if (this._componentRef) {
-      this._componentRef.instance.scrollToTop();
-    }
+    this.scrollable.scrollTo({ top: 0, behavior: 'smooth' });
   }
 
 }
