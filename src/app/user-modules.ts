33924236b4af5158015689558a4752355ebb4856
Edmunds Folkmanis <folkmanis@gmail.com>
2021-05-25 06:01:42 +0000

    removed new repro job

--- src/app/user-modules.ts
+++ src/app/user-modules.ts
@@ -5,7 +5,6 @@ export const USER_MODULES: UserModule[] = [
     { name: 'Pievienot XMF arhīvu', description: 'XFM arhīva jaunināšana', route: 'xmf-upload' },
     {
         name: 'Darbi', description: 'Ražošanas darbi', route: 'jobs', childMenu: [
-            { name: 'Jauns repro pasūtījums', route: 'repro/new', description: 'Izveidot jaunu repro darbu' },
             { name: 'Repro darbi', route: 'repro', description: 'Repro darbu saraksts' },
         ]
     },
