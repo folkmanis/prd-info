31864b2e13f5e591f831f3dded47a362d2e20840
Edmunds Folkmanis <folkmanis@gmail.com>
2021-06-19 07:23:08 +0000

    navigation handled by router

--- src/app/library/errors/errors.service.ts
+++ src/app/library/errors/errors.service.ts
@@ -1,4 +1,6 @@
-import { Injectable, ErrorHandler } from '@angular/core';
+import { Injectable, ErrorHandler, NgZone } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 
@@ -6,18 +8,19 @@ import { Observable, throwError } from 'rxjs';
 export class ErrorsService implements ErrorHandler {
 
   constructor(
+    private router: Router,
+    private zone: NgZone,
   ) { }
 
   handleError(error: HttpErrorResponse) {
     if (error instanceof HttpErrorResponse) {
       if (error.status === 401) {
-        location.pathname = '/login';
+        this.zone.run(() => this.router.navigate(['/login']));
       }
     }
-    console.error('Error ', error);
     if (error.error instanceof ErrorEvent) {
     } else {
-      console.error(`Status code ${error.status}, body: ${error.error}`);
+      console.error(`Status code ${error.status}, body:`, error.error);
     }
     return throwError('Kļūda!');
   }
