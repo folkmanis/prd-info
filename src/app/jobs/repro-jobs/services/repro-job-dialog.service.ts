6a4b4298138accfdb084270a8a19a69490043337
Edmunds Folkmanis <folkmanis@gmail.com>
2021-07-13 09:25:50 +0000

    added job edit by Id function

--- src/app/jobs/repro-jobs/services/repro-job-dialog.service.ts
+++ src/app/jobs/repro-jobs/services/repro-job-dialog.service.ts
@@ -4,6 +4,10 @@ import { JobBase } from 'src/app/interfaces';
 import { ReproJobEditComponent } from '../repro-job-edit/repro-job-edit.component';
 import { JobFormGroup } from './job-form-group';
 import { CustomersService, ProductsService } from 'src/app/services';
+import { EMPTY, Observable, of } from 'rxjs';
+import { JobService } from 'src/app/services/job.service';
+import { concatMap, map, switchMap } from 'rxjs/operators';
+import { endOfDay } from 'date-fns';
 
 export interface DialogData {
   form: JobFormGroup;
@@ -25,6 +29,7 @@ export class ReproJobDialogService {
     private matDialog: MatDialog,
     private customersService: CustomersService,
     private productsService: ProductsService,
+    private jobService: JobService,
   ) { }
 
   openJob(job: Partial<JobBase>): MatDialogRef<ReproJobEditComponent, DialogData> {
@@ -41,6 +46,18 @@ export class ReproJobDialogService {
 
     return this.matDialog.open<ReproJobEditComponent, DialogData, DialogData>(ReproJobEditComponent, config);
 
+  }
+
+  editJob(jobId: number): Observable<boolean> {
+    return this.jobService.getJob(jobId).pipe(
+      concatMap(job => this.openJob(job).afterClosed()),
+      concatMap(data => data ? of(data.job) : EMPTY),
+      map(job => ({
+        ...job,
+        dueDate: endOfDay(new Date(job.dueDate)),
+      })),
+      concatMap(job => this.jobService.updateJob(job)),
+    );
   }
 
   private setJobDefaults<T extends Partial<JobBase>>(job: T): T {
