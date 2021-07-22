62eeff5d1e39443e804d0b5f325361e024fe0ae9
Edmunds Folkmanis <folkmanis@gmail.com>
2021-07-22 11:24:25 +0000

    clean-up

--- src/app/jobs/repro-jobs/job-list/job-list.component.ts
+++ src/app/jobs/repro-jobs/job-list/job-list.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit, ChangeDetectorRef } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { filter, map, switchMap, takeUntil, tap } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { DestroyService } from 'prd-cdk';
+import { OperatorFunction, pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { JobBase, JobProduct } from 'src/app/interfaces';
 import { LayoutService } from 'src/app/layout/layout.service';
-import { DestroyService } from 'prd-cdk';
 import { ClipboardService } from 'src/app/library/services/clipboard.service';
 import { JobService } from 'src/app/services/job.service';
 
@@ -16,37 +16,20 @@ import { JobService } from 'src/app/services/job.service';
 })
 export class JobListComponent implements OnInit {
 
-  @Input('editorActive') set active(active: boolean) {
-    this._active = active;
-  }
-  get active(): boolean {
-    return this._active;
-  }
-  private _active = false;
+  isLarge$ = this.layout.isLarge$;
+
+  dataSource$ = this.jobService.jobs$.pipe(
+    addProductsInformation(),
+  );
 
   constructor(
     private jobService: JobService,
     private clipboard: ClipboardService,
     private layout: LayoutService,
-    private destroy$: DestroyService,
-    private changeDetector: ChangeDetectorRef,
   ) { }
 
 
-  isLarge = true;
-
-  dataSource$ = this.jobService.jobs$.pipe(
-    map(jobs => jobs.map(job => ({
-      ...job,
-      productsObj: this.combineProducts(job.products),
-    }))),
-  );
-
   ngOnInit(): void {
-    this.layout.isLarge$.pipe(
-      tap(_ => this.changeDetector.markForCheck()),
-      takeUntil(this.destroy$),
-    ).subscribe(large => this.isLarge = large);
   }
 
   copyJobIdAndName(job: Pick<JobBase, 'jobId' | 'name'>, event: MouseEvent) {
@@ -64,12 +47,30 @@ export class JobListComponent implements OnInit {
     }).subscribe();
   }
 
-  private combineProducts(products: JobProduct[] | JobProduct): Pick<JobProduct, 'name'> {
-    if (!(products instanceof Array)) { return products; }
-    if (products.length === 1) { return products[0]; }
-    return {
-      name: products.map(prod => prod.name).join(', '),
-    };
-  }
 
+}
+
+function addProductsInformation(): OperatorFunction<JobBase[], (JobBase & { productsObj: Partial<JobProduct>; })[]> {
+
+  return pipe(
+    map(jobs =>
+      jobs.map(job => ({ ...job, productsObj: productsObj(job.products), }))
+    ),
+  );
+
+}
+
+function productsObj(products: JobProduct | JobProduct[] | undefined): Partial<JobProduct> {
+  if (!products) {
+    return { name: '' };
+  }
+  if (!(products instanceof Array)) {
+    return { ...products };
+  }
+  if (products.length === 1) {
+    return { ...products[0] };
+  }
+  return {
+    name: products.map(prod => prod.name).join(', '),
+  };
 }
