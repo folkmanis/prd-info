5f9d7593987dc69cdc67658b28bc936bc7af760c
Edmunds Folkmanis <folkmanis@gmail.com>
2021-08-10 11:52:07 +0000

    added messaging and notifications services

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -3,3 +3,5 @@ export * from './customers.service';
 export * from './products.service';
 export * from './system-preferences.service';
 export * from './login.service';
+export * from './messaging.service';
+export * from './notifications.service';
