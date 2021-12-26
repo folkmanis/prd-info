469ed83447827b8c57014d7aa0e329944b474f97
Edmunds Folkmanis <folkmanis@gmail.com>
2021-12-26 19:31:35 +0000

    combine-reload moved to library

--- src/app/admin/logfile/log-filter/log-filter.component.ts
+++ src/app/admin/logfile/log-filter/log-filter.component.ts
@@ -1,26 +1,11 @@
 import { ChangeDetectionStrategy, Component, Input, Output } from '@angular/core';
 import { isEqual } from 'lodash';
 import moment from 'moment';
-import { merge, Observable, ReplaySubject, Subject } from 'rxjs';
-import { distinctUntilChanged, map, scan, withLatestFrom } from 'rxjs/operators';
+import { Observable, ReplaySubject, Subject } from 'rxjs';
+import { distinctUntilChanged, map, withLatestFrom } from 'rxjs/operators';
+import { combineReload } from 'src/app/library/rxjs';
 import { LogQueryFilter } from '../../services/logfile-record';
 import { ValidDates } from '../valid-dates.class';
-
-
-function combineReload<T>(data$: Observable<T>, reload$: Observable<void>): Observable<T> {
-  return merge(
-    data$,
-    reload$
-  ).pipe(
-    scan((initial, val) => {
-      if (!initial && !val) {
-        throw new Error('Value must be supplied before reload');
-      }
-      return val || initial;
-    }),
-    map(value => value as T),
-  );
-}
 
 
 @Component({
