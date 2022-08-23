f3b41665d15bcf702dfae421415714e07e186ce3
Edmunds Folkmanis <folkmanis@gmail.com>
2022-08-23 11:20:09 +0300

    cleanup

--- src/app/jobs-admin/customers/customers.module.ts
+++ src/app/jobs-admin/customers/customers.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LibraryModule } from 'src/app/library/library.module';
-import { SimpleFormModule } from 'src/app/library/simple-form';
 import { CustomerEditComponent } from './customer-edit/customer-edit.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
 import { CustomerResolverService } from './services/customer-resolver.service';
