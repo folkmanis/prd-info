b51fd323acb98becc4b99cd279d85f3b3400e6f1
Edmunds Folkmanis <folkmanis@gmail.com>
2022-08-23 17:32:40 +0300

    custom class transformer

--- src/app/services/prd-api/materials-api.service.ts
+++ src/app/services/prd-api/materials-api.service.ts
@@ -1,10 +1,10 @@
 import { AppParams, Material } from 'src/app/interfaces';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map, OperatorFunction } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { HttpOptions } from 'src/app/library/http/http-options';
 import { Injectable, Inject } from '@angular/core';
 import { APP_PARAMS } from 'src/app/app-params';
-import { ClassTransformer } from 'class-transformer';
+import { AppClassTransformerService } from 'src/app/library';
 
 @Injectable({
     providedIn: 'root'
@@ -16,41 +16,41 @@ export class MaterialsApiService {
     constructor(
         private http: HttpClient,
         @Inject(APP_PARAMS) private params: AppParams,
-        private transformer: ClassTransformer,
+        private transformer: AppClassTransformerService,
     ) { }
 
     getOne(id: string): Observable<Material> {
-        return this.http.get(this.path + id, new HttpOptions()).pipe(
-            this.toClass<Record<string, any>>(),
+        return this.http.get<Record<string, any>>(this.path + id, new HttpOptions()).pipe(
+            this.transformer.toClass(Material),
         );
     }
 
     getAll(params: Record<string, any> = {}): Observable<Material[]> {
-        return this.http.get(
+        return this.http.get<Record<string, any>[]>(
             this.path,
             new HttpOptions(params)
         ).pipe(
-            this.toClass(),
+            this.transformer.toClass(Material),
         );
     }
 
     updateOne(id: string, data: Partial<Material>, params?: Record<string, any>): Observable<Material> {
-        return this.http.patch(
+        return this.http.patch<Record<string, any>>(
             this.path + id,
             data,
             new HttpOptions(params)
         ).pipe(
-            this.toClass<Record<string, any>>(),
+            this.transformer.toClass(Material),
         );
     }
 
     insertOne(data: Partial<Material>, params?: Record<string, any>): Observable<Material> {
-        return this.http.put(
+        return this.http.put<Record<string, any>>(
             this.path,
             data,
             new HttpOptions(params)
         ).pipe(
-            this.toClass<Record<string, any>>(),
+            this.transformer.toClass(Material),
         );
     }
 
@@ -58,12 +58,6 @@ export class MaterialsApiService {
         return this.http.get<Material[K][]>(this.path + 'validate/' + key, new HttpOptions().cacheable());
     }
 
-
-    private toClass<T extends Record<string, any>[]>(): OperatorFunction<T, Material[]>;
-    private toClass<T extends Record<string, any>>(): OperatorFunction<T, Material>;
-    private toClass<T extends Record<string, any>>(): OperatorFunction<T, Material> | OperatorFunction<T[], Material[]> {
-        return map(data => this.transformer.plainToInstance(Material, data, { exposeDefaultValues: true }));
-    }
 
 
 }
