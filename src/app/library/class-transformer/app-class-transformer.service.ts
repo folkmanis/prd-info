0e125c4396c0ff2cd8b2c71fcb42a1fe50018e03
Edmunds Folkmanis <folkmanis@gmail.com>
2022-09-01 12:53:09 +0300

    options added

--- src/app/library/class-transformer/app-class-transformer.service.ts
+++ src/app/library/class-transformer/app-class-transformer.service.ts
@@ -9,9 +9,9 @@ export * from 'class-transformer';
 })
 export class AppClassTransformerService extends ClassTransformer {
 
-  plainToInstance<T extends Record<string, any>, V extends any[]>(cls: ClassConstructor<T>, plain: V, options?: ClassTransformOptions): T[];
-  plainToInstance<T extends Record<string, any>, V>(cls: ClassConstructor<T>, plain: V, options?: ClassTransformOptions): T;
-  plainToInstance<T extends Record<string, any>>(cls: ClassConstructor<T>, plain: unknown, options?: ClassTransformOptions): T | T[] {
+  plainToInstance<T extends Record<string, any>, V extends any[]>(cls: ClassConstructor<T>, plain: V, options: ClassTransformOptions): T[];
+  plainToInstance<T extends Record<string, any>, V>(cls: ClassConstructor<T>, plain: V, options: ClassTransformOptions): T;
+  plainToInstance<T extends Record<string, any>>(cls: ClassConstructor<T>, plain: unknown, options: ClassTransformOptions = {}): T | T[] {
     options = {
       exposeDefaultValues: true,
       ...options,
@@ -19,10 +19,10 @@ export class AppClassTransformerService extends ClassTransformer {
     return super.plainToInstance(cls, plain, options);
   }
 
-  toClass<T extends Record<string, any>, V extends Array<any>, U = T[]>(obj: ClassConstructor<T>): OperatorFunction<V, U>;
-  toClass<T extends Record<string, any>, V, U = T>(obj: ClassConstructor<T>): OperatorFunction<V, U>;
-  toClass<T extends Record<string, any>, V>(obj: ClassConstructor<T>): OperatorFunction<V, T | T[]> {
-    return map((data) => this.plainToInstance(obj, data, { exposeDefaultValues: true }));
+  toClass<T extends Record<string, any>, V extends Array<any>, U = T[]>(obj: ClassConstructor<T>, options?: ClassTransformOptions): OperatorFunction<V, U>;
+  toClass<T extends Record<string, any>, V, U = T>(obj: ClassConstructor<T>, options?: ClassTransformOptions): OperatorFunction<V, U>;
+  toClass<T extends Record<string, any>, V>(obj: ClassConstructor<T>, options?: ClassTransformOptions): OperatorFunction<V, T | T[]> {
+    return map((data) => this.plainToInstance(obj, data, options));
   }
 
 
