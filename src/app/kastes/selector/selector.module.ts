f0b4a3ee77b12f0788a2bda1401c8eda4d3bf790
Edmunds Folkmanis <folkmanis@gmail.com>
2022-10-05 11:27:42 +0300

    moved components

--- src/app/kastes/selector/selector.module.ts
+++ src/app/kastes/selector/selector.module.ts
@@ -6,9 +6,8 @@ import { SelectorRoutingModule } from './selector-routing.module';
 import { SelectorComponent } from './selector.component';
 import { LabelsComponent } from './labels/labels.component';
 import { TabulaComponent } from './tabula/tabula.component';
-import { ColorsOutputComponent } from './tabula/kopskaiti/colors-output/colors-output.component';
-import { KopskaitiComponent } from './tabula/kopskaiti/kopskaiti.component';
-import { RefreshTableComponent } from './tabula/refresh-table/refresh-table.component';
+import { ColorsOutputComponent } from './kopskaiti/colors-output/colors-output.component';
+import { KopskaitiComponent } from './kopskaiti/kopskaiti.component';
 import { RowIdDirective } from './tabula/row-id.directive';
 import { OrderTotalsComponent } from './order-totals/order-totals.component';
 import { MaterialLibraryModule } from 'src/app/library/material-library.module';
@@ -23,7 +22,6 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
     TabulaComponent,
     ColorsOutputComponent,
     KopskaitiComponent,
-    RefreshTableComponent,
     RowIdDirective,
     OrderTotalsComponent,
   ],
