047a52d0aa3ac8bdbb5d3d917cd118c421531756
Edmunds Folkmanis <folkmanis@gmail.com>
2023-06-02 17:21:41 +0300

    cleanup

--- src/app/jobs/jobs.module.ts
+++ src/app/jobs/jobs.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 import { MaterialLibraryModule } from 'src/app/library/material-library.module';
 import { LibraryModule } from '../library/library.module';
 import { JobsRoutingModule } from './jobs-routing.module';
-import { JobsComponent } from './jobs.component';
 import { ReproJobsModule } from './repro-jobs/repro-jobs.module';
 
 
 @NgModule({
-  declarations: [
-  ],
   imports: [
-    JobsComponent,
     CommonModule,
     LibraryModule,
     MaterialLibraryModule,
