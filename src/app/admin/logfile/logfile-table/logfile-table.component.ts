81cf0d0a536341ab5988a204a47986f3d63a9042
Edmunds <folkmanis@gmail.com>
2025-08-23 23:47:36 +0300

    refactor: improve structure and styling of logfile table component animations

--- src/app/admin/logfile/logfile-table/logfile-table.component.ts
+++ src/app/admin/logfile/logfile-table/logfile-table.component.ts
@@ -1,4 +1,3 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
 import { DatePipe, JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
@@ -11,19 +10,12 @@ import { ViewSizeDirective } from 'src/app/library/view-size';
   templateUrl: './logfile-table.component.html',
   styleUrls: ['./logfile-table.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0' })),
-      state('expanded', style({ height: '*' })),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
   imports: [MatTableModule, JsonPipe, DatePipe, ShortenTextPipe, ViewSizeDirective],
 })
 export class LogfileTableComponent {
-  readonly displayedColumns = ['level', 'timestamp', 'info', 'metadata'];
+  protected readonly displayedColumns = ['level', 'timestamp', 'info', 'metadata'];
 
-  expandedRecord: LogRecord | null;
+  protected expandedRecord: LogRecord | null = null;
 
   log = input<LogRecord[]>([]);
 }
