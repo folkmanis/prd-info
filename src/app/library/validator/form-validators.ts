9d7fd2b360e423ba9581378095c40d797f7c803e
Edmunds <folkmanis@gmail.com>
2026-07-11 22:11:39 +0300

    update working

--- src/app/library/validator/form-validators.ts
+++ src/app/library/validator/form-validators.ts
@@ -2,6 +2,6 @@ import { SchemaPath, validateStandardSchema } from '@angular/forms/signals';
 import { z } from 'zod';
 
 export function positiveNumericString(path: SchemaPath<string>) {
-    validateStandardSchema(path, z.coerce.number().positive());
+    validateStandardSchema(path, z.coerce.number().nonnegative());
 }
 
